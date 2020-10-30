bb4104309622dbefffbe8ee8c0b0dce1df5a210b
ophelia swen <ophelia.swen@gmail.com>
2020-10-30 10:16:03 -0700

    add a put spec as well

--- spec/requests/put_animals_spec.rb
+++ spec/requests/put_animals_spec.rb
@@ -1,17 +1,17 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# describe "put all names route", :type => :request do
-#   let!(:names) { FactoryBot.create_list(:name, 20)}
+describe "put all animals route", :type => :request do
+  let!(:animals) { FactoryBot.create_list(:animal, 20)}
 
-#   before do
-#     put '/names/5', params: { :petName => 'test_petName', :petType => 'test_petType', :petColor => 'test_petColor'} 
-#   end  
+  before do
+    put '/animals/1', params: { :catName => 'test_catName', :dogName => 'test_dogName', :catType => 'test_catType', :dogType => 'test_dogType', :dogSize => 'test_dogSize'}
+  end  
 
-#   it 'updates the body of name' do
-#     expect(JSON.parse(response.body)['message']).to eq 'This name has been updated successfully.'
-#   end
+  it 'updates the body of animal' do
+    expect(JSON.parse(response.body)['message']).to eq 'This animal has been updated successfully.'
+  end
 
-#   it 'returns status code 200' do
-#     expect(response).to have_http_status(:success)
-#   end
-# end
+  it 'returns status code 200' do
+    expect(response).to have_http_status(:success)
+  end
+end
